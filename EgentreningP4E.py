5634ae0ee46f96c48704b74e24399a6c59fafe91
Magnus <70261216+maaskeie@users.noreply.github.com>
2020-08-26 13:07:58 +0200

    Add files via upload

--- EgentreningP4E.py
+++ EgentreningP4E.py
@@ -20,4 +20,25 @@ while count < 10:
     count += 1
     print(count)
     
+# Hvilket år vil et bankinnskudd på 50.000 kroner til 5% årlig rente
+# bli likt eller høyere enn et bankinnskudd på 100.000 kroner til 2 % årlig rente? 
+INNSKUDD_A = 50000
+INNSKUDD_B = 100000
+RENTE_A = 0.05
+RENTE_B = 0.02
+balanse_a = INNSKUDD_A
+balanse_b = INNSKUDD_B
+renteinntekt_a = balanse_a * RENTE_A
+renteinntekt_b = balanse_b * RENTE_B
+aar = 0
+
+while balanse_a < balanse_b:
+    balanse_a = balanse_a + renteinntekt_a
+    balanse_b = balanse_b + renteinntekt_b
+    aar += 1
+#    print(balanse_a, end="  ")
+#    print(balanse_b, end="  ")
+#    print(aar)
+print("Det tar", aar, "år før det lavere bankinnskuddet blir minst like høyt som det andre")
+    
 
